dc8c4625cf05aa46bab4f10720608b76d8ce02b5
lbenav8095 <lubensaico@gmail.com>
2022-11-30 15:20:15 -0600

    Initial commit

---
+++ src/main/java/us/mytheria/blobrp/BlobRP.java
@@ -0,0 +1,17 @@
+package us.mytheria.blobrp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlobRP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
