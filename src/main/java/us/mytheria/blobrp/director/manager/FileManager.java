fb6d05e524365248d33b406512426fca7817fb9e
lbenav8095 <lubensaico@gmail.com>
2023-01-25 13:11:42 -0600

    FileManager

--- src/main/java/us/mytheria/blobrp/director/manager/FileManager.java
+++ src/main/java/us/mytheria/blobrp/director/manager/FileManager.java
@@ -2,7 +2,6 @@ package us.mytheria.blobrp.director.manager;
 
 import org.bukkit.configuration.file.YamlConfiguration;
 import us.mytheria.bloblib.BlobLibDevAPI;
-import us.mytheria.bloblib.utilities.ResourceUtil;
 import us.mytheria.blobrp.BlobRP;
 import us.mytheria.blobrp.director.RPManager;
 import us.mytheria.blobrp.director.RPManagerDirector;
@@ -26,29 +25,16 @@ public class FileManager extends RPManager {
         path = new File("plugins/BlobRP");
         files = new HashMap<>();
         files.put("ShopArticles", new File(path.getPath() + "/articles"));
-        files.put("Inventories", new File(path.getPath() + "/inventories.yml"));
 
         try {
             if (!path.exists()) path.mkdir();
-            if (!getShopArticles().exists()) getShopArticles().mkdir();
-            ///////////////////////////////////////////
-            if (!inventoriesFile().exists()) inventoriesFile().createNewFile();
-            ResourceUtil.updateYml(path, "/tempInventories.yml", "inventories.yml", inventoriesFile(), main);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public File inventoriesFile() {
-        return files.get("Inventories");
-    }
-
     public File getFile(String key) {
         return files.get(key);
-    }
-
-    public File getShopArticles() {
-        return getFile("ShopArticles");
     }
 
     public YamlConfiguration getYml(File f) {
