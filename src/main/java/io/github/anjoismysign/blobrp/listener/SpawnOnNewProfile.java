1fb72f24b9fd3e9d5d5d49939b4ff99b97b2b916
anjoismysign <lubensaico@gmail.com>
2026-01-12 16:23:17 -0600

    Add SpawnOnRespawn and SpawnOnNewProfile listeners for roleplay warp.

---
+++ src/main/java/io/github/anjoismysign/blobrp/listener/SpawnOnNewProfile.java
@@ -0,0 +1,37 @@
+package io.github.anjoismysign.blobrp.listener;
+
+import io.github.anjoismysign.bloblib.events.ProfileLoadEvent;
+import io.github.anjoismysign.blobrp.BlobRPAPI;
+import io.github.anjoismysign.blobrp.director.manager.ConfigManager;
+import io.github.anjoismysign.blobrp.entity.configuration.RoleplayWarpConfiguration;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.Nullable;
+
+public class SpawnOnNewProfile extends RPListener {
+
+    public SpawnOnNewProfile(ConfigManager configManager) {
+        super(configManager);
+    }
+
+    public void reload() {
+        HandlerList.unregisterAll(this);
+        if (getConfigManager().getSpawnOnNewProfile().register() && RoleplayWarpConfiguration.getInstance().isEnabled()) {
+            Bukkit.getPluginManager().registerEvents(this, getConfigManager().getPlugin());
+        }
+    }
+
+    @EventHandler
+    public void onLoad(ProfileLoadEvent event) {
+        var profile = event.getProfile();
+        if (profile.hasPlayedBefore()){
+            return;
+        }
+        @Nullable var warpAsSpawn = RoleplayWarpConfiguration.getInstance().roleplayWarpAsSpawn();
+        if (warpAsSpawn == null){
+            return;
+        }
+        BlobRPAPI.getInstance().getWarp(warpAsSpawn).warp(event.getPlayer());
+    }
+}
---
+++ src/main/java/io/github/anjoismysign/blobrp/listener/SpawnOnRespawn.java
@@ -0,0 +1,38 @@
+package io.github.anjoismysign.blobrp.listener;
+
+import io.github.anjoismysign.blobrp.BlobRPAPI;
+import io.github.anjoismysign.blobrp.director.manager.ConfigManager;
+import io.github.anjoismysign.blobrp.entity.Spectator;
+import io.github.anjoismysign.blobrp.entity.configuration.RoleplayWarpConfiguration;
+import io.github.anjoismysign.blobrp.event.SpectatorStartEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.jetbrains.annotations.Nullable;
+
+public class SpawnOnRespawn extends RPListener {
+
+    public SpawnOnRespawn(ConfigManager configManager) {
+        super(configManager);
+    }
+
+    public void reload() {
+        HandlerList.unregisterAll(this);
+        if (getConfigManager().getSpawnOnRespawn().register() && RoleplayWarpConfiguration.getInstance().isEnabled()) {
+            Bukkit.getPluginManager().registerEvents(this, getConfigManager().getPlugin());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        @Nullable var warpAsSpawn = RoleplayWarpConfiguration.getInstance().roleplayWarpAsSpawn();
+        if (warpAsSpawn == null){
+            return;
+        }
+        BlobRPAPI.getInstance().getWarp(warpAsSpawn).warp(event.getPlayer());
+    }
+}
